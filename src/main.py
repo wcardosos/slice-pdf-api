c0c5fddc557fd0b1bc1212195dea04d55d6771dd
Wagner Cardoso <wagnerdev01@gmail.com>
2022-03-14 16:40:24 -0300

    refactor(main) Fix lint problems

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, Response
 import json
+from fastapi import FastAPI, Response
 
 
 app = FastAPI()
 
+
 @app.get('/health-check')
 def health_check():
+    '''
+        Health check endpoint
+    '''
     return Response(content=json.dumps({}))
