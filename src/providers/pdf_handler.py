5eb10aea56027179c52c634b19d4a3095432d060
Wagner Cardoso <wagnerdev01@gmail.com>
2022-03-15 15:59:12 -0300

    feat(providers/PDFHandler) Add method to create pdf from images

--- src/providers/pdf_handler.py
+++ src/providers/pdf_handler.py
@@ -1,6 +1,7 @@
 # pylint: disable=import-error
 from os.path import exists as file_exists
 from pdf2image import convert_from_path
+from fpdf import FPDF
 from src.errors.pdf.pdf_file_not_found_exception import (
     PDFFileNotFoundException
 )
@@ -10,8 +11,9 @@ class PDFHandler:
     '''
         Class to handler PDF files.
     '''
+
     @staticmethod
-    def extract_pages_to_images(filepath: str) -> None:
+    def extract_pages_to_images(filepath: str) -> list:
         '''
             Extract PDF's pages as images and save it.
         '''
@@ -19,6 +21,22 @@ class PDFHandler:
             raise PDFFileNotFoundException()
 
         pages = convert_from_path(filepath)
+        filename_list = []
 
         for index, page in enumerate(pages):
-            page.save(f'page{index + 1}.jpg', 'JPEG')
+            filename = f'page{index + 1}.jpg' 
+            page.save(filename, 'JPEG')
+            filename_list.append(filename)
+        
+        return filename_list
+    
+    @staticmethod
+    def create_from_images(images_list: list, filename: str) -> None:
+        pdf = FPDF()
+
+        for image in images_list:
+            pdf.add_page()
+            pdf.image(image, 0, 0, pdf.w, pdf.h)
+        
+        pdf.output(filename, 'F')
+        pdf.close()
