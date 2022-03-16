6e02a174176260bdd31481e998268c6612d271a7
Wagner Cardoso <wagnerdev01@gmail.com>
2022-03-16 15:32:38 -0300

    feat(main) Add slice endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
-from controllers.health_check import HealthCheckController
+from fastapi import FastAPI, UploadFile
+from src.controllers.health_check import HealthCheckController
+from src.controllers.slice import SliceController
 
 
 app = FastAPI()
+
+
+@app.post('/slice')
+async def slice_pdf(pdf_file: UploadFile):
+    '''
+        Slice PDF endpoint
+    '''
+    return await SliceController.post(pdf_file)
 
 
 @app.get('/health-check')
